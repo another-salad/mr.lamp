b9eb0acce44dee9b3396d6ac5685f36c3f6b2dce
another Salad <nothanks@nothanks.com>
2023-04-27 23:24:06 +0100

    testing enc/dec

--- src/code.py
+++ src/code.py
@@ -4,16 +4,16 @@ import aesio
 import os
 from binascii import hexlify
 
-from wiznet5keth import NetworkConfig, config_eth, wsgi_web_server
-from config_utils import get_config_from_json_file
+# from wiznet5keth import NetworkConfig, config_eth, wsgi_web_server
+# from config_utils import get_config_from_json_file
 
 
-secrets = get_config_from_json_file(".secrets.json")  # Yeah lets just put all these in memory, yolo...
+# secrets = get_config_from_json_file(".secrets.json")  # Yeah lets just put all these in memory, yolo...
 
 # WebServer config and setup
-eth_interface = config_eth(NetworkConfig(**get_config_from_json_file()))
-wsgi_server, web_app = wsgi_web_server(eth_interface)
-wsgi_server.start()
+# eth_interface = config_eth(NetworkConfig(**get_config_from_json_file()))
+# wsgi_server, web_app = wsgi_web_server(eth_interface)
+# wsgi_server.start()
 
 class UnsupportedTypeException(Exception):
     """Usual shouty shouty Python"""
@@ -27,19 +27,29 @@ def _validate(data):
 
     return data
 
-def gen_iv(size=16):
+def gen_bytes(size=16):
     """Waffer thin wrapper for clarity"""
     return os.urandom(size)
 
-def _crpyto_operation(key, message, method, iv_size=16):
+def decrypt(key, message, iv_size=16):
     message = _validate(message)
     iv = message[-iv_size:]
     message = message[:-iv_size]
     cipher = aesio.AES(key, aesio.MODE_CTR, iv)
-    call = getattr(cipher, method)
     output_buff = bytearray(len(message))
-    call(message, output_buff)
-    return output_buff.decode()
+    cipher.decrypt_into(message, output_buff)
+    return bytes(output_buff)
+
+def encrypt(key, message, iv=None):
+    """if IV is None, generates IV from gen_bytes."""
+    message = _validate(message)
+    output_buff = bytearray(len(message))
+    if not iv:
+        iv = gen_bytes()
+    cipher = aesio.AES(key, aesio.MODE_CTR, iv)
+    cipher.encrypt_into(message, output_buff)
+    output_buff = bytes(output_buff) + iv
+    return output_buff
 
 def to_morse(data):
     pass
